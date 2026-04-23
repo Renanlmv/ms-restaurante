58be959e091f6a9addb7468dcc305e52736d82c1
Renanlmv <renanlmv12@gmail.com>
2026-04-23 17:16:22 -0300

    feat: adiciona métodos GET personalizados ao repositório de reservas

--- src/main/java/com/github/renanlmv/ms/restaurante/repositories/ReservaRepository.java
+++ src/main/java/com/github/renanlmv/ms/restaurante/repositories/ReservaRepository.java
@@ -3,5 +3,14 @@ package com.github.renanlmv.ms.restaurante.repositories;
 import com.github.renanlmv.ms.restaurante.entities.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    // metodo para listar todas as reservas pelo id do restaurante
+    List<Reserva> findByRestauranteId (Long restauranteId);
+
+    // metodo personalizado para encontrar uma reserva pelo seu id e o id do restaurante
+    Optional<Reserva> findByIdAndRestauranteId(Long reservaId, Long restauranteId);
 }
